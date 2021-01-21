7aa0795c98c7267a452b60210d335e350911e52a
Hyevvy <hyeg0@naver.com>
2021-01-21 19:05:23 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const { default: AppLayout } = require("../components/AppLayout")
+
+const Home = () => {{
+    return(
+        <AppLayout>
+            <div>Hello</div>
+        </AppLayout>
+    );
+}}
+export default Home;
