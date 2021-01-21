9c2530d2292724cd2d6f59f08330626012702b7c
Hyevvy <hyeg0@naver.com>
2021-01-21 21:17:40 +0900

    front page

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,14 +1,44 @@
 import PropTypes from 'prop-types'
 import Link from 'next/link';
+import {Menu, Input,Row,Col } from 'antd';
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+import {useState} from 'react'
+import styled from 'styled-components'
+
+const SearchInput = styled(Input.Search)`
+    verticalAlgin:middle
+`;
+
 const AppLayout = ({children}) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
     <div>
-        <div>
-            <Link href="/"><a>노드버드</a></Link>
-            <Link href="/profile"><a>프로필</a></Link>
-            <Link href="/signup"><a>회원가입</a></Link>
-        </div>
-        {children}
+        <Menu mode="horizontal">
+            <Menu.Item>
+             <Link href="/"><a>노드버드</a></Link>
+            </Menu.Item>
+            <Menu.Item>
+             <Link href="/profile"><a>프로필</a></Link>
+            </Menu.Item>
+            <Menu.Item>
+             <SearchInput enterButton/>
+            </Menu.Item>
+            <Menu.Item>
+             <Link href="/signup"><a>회원가입</a></Link>
+            </Menu.Item>
+        </Menu>
+        <Row gutter={8}>
+            <Col xs={24} md={6}>
+                {isLoggedIn ? <UserProfile /> : <LoginForm />}
+            </Col>
+            <Col xs={24} md={12}>
+              {children}
+            </Col>
+            <Col xs={24} md={6}>
+                <a href="https://github.com/Hyevvy" target="_blank" rel="noreferrer noopener"> made by Hyevvy</a>
+            </Col>
+        </Row>
     </div>
     )
 };
