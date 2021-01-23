54ffa2a8d22ca8bf624c74394d9023e1b2562574
Hyevvy <hyeg0@naver.com>
2021-01-23 15:42:04 +0900

    next redux wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
-
+import wrapper from '../store/configureStore'
 const NodeBird = ({Component}) => {
     return(
         <>
@@ -16,4 +16,4 @@ const NodeBird = ({Component}) => {
 NodeBird.propTypes ={
     Component:PropTypes.elementType.isRequired,
 }
-export default NodeBird;
+export default wrapper.withRedux(NodeBird);
